77782211591f23db56e1cbde9ab0da046ac6fe6f
Diego Canales <diegoc1@stanford.edu>
2013-11-24 14:46:34 -0800

    comitting random changes before pull (line classifying stuff)

--- TesseractSample/TesseractSample/LineClassifier.m
+++ TesseractSample/TesseractSample/LineClassifier.m
@@ -15,14 +15,14 @@
     self = [super init];
     if (self) {
         NSLog(@"CREATING with strings %@", trainingData);
-        [self extractFeaturesFromLine:trainingData[0]];
+      //  [self extractFeaturesFromLine:trainingData[0]];
     }
     return self;
 }
 
 - (NSMutableArray *) extractFeaturesFromLine:(NSString *)line {
     NSLog(@"extracting features for line %@", line);
-    
+    return nil;
     
 }
 
